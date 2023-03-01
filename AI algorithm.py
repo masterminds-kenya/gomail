dceaaf7b9e172a495427b9b64a85ee7f4428fb74
mmulika <mulikamike@gmail.com>
2023-03-01 19:19:11 +0300

    q-learning

--- AI algorithm.py
+++ AI algorithm.py
@@ -21,3 +21,37 @@ model = tf.keras.Sequential([
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, epochs=150, batch_size=10)
 predictions = model.predict(X_test)
+
+
+import numpy as np
+
+# Initialize Q-table with zeros
+Q = np.zeros([num_states, num_actions])
+
+# Set learning parameters
+alpha = 0.1
+gamma = 0.9
+epsilon = 0.1
+
+# Loop over episodes
+for episode in range(num_episodes):
+    # Reset environment and get initial state
+    state = env.reset()
+    done = False
+    
+    # Loop over time steps in current episode
+    while not done:
+        # Choose action based on epsilon-greedy policy
+        if np.random.rand() < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[state, :])
+        
+        # Take action and observe next state and reward
+        next_state, reward, done, _ = env.step(action)
+        
+        # Update Q-value for current state-action pair
+        Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[next_state, :]) - Q[state, action])
+        
+        # Update state
+        state = next_state
